94581e84a0d639ca9e985b9406ac16abea0cfda5
suraj270702 <surajmaurya2707@eng.rizvi.edu.in>
2023-07-16 15:37:43 +0530

    code for leetcode problem 942

---
+++ DIstringmatch.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int[] perm = new int[n + 1];
+
+        int start = 0;
+        int end = n;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'I') {
+                perm[i] = start++;
+            } else {
+                perm[i] = end--;
+            }
+        }
+        perm[n] = start;
+
+        return perm;
+    }
+}
